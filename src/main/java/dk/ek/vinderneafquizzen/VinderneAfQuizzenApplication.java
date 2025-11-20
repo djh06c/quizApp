1be582c40d7c2f0ccf840a889e873c48f3fb1001
djh06c <djdavidjorgen@gmail.com>
2025-11-20 11:54:06 +0100

    first commmit

---
+++ src/main/java/dk/ek/vinderneafquizzen/VinderneAfQuizzenApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.vinderneafquizzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinderneAfQuizzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinderneAfQuizzenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/ek/vinderneafquizzen/VinderneAfQuizzenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.vinderneafquizzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinderneAfQuizzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
